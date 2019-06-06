b6b3639a4b23701e2c99c784a43e5c7410071435
xuejuntao <xuejuntao@baidu.com>
2019-06-06 22:20:15 +0800

    init

--- example/test.js
+++ example/test.js
@@ -6,6 +6,33 @@
 /* jshint node:true */
 /* jshint esversion:8 */
 /* jshint laxbreak:true */
+/* jshint loopfunc:true */
 'use strict';
 
 const WaitingQueue = require('../index').WaitingQueue;
+
+
+let queue = new WaitingQueue({
+    concurrent: 2
+});
+
+function process(i) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(i);
+        }, 3000);
+    });
+}
+const max = 5;
+for(let i = 0; i < max; i++) {
+    queue.add({
+        id: i,
+        callback: process
+    }, {
+        waitTimeout: 1000
+    }).then(id => {
+        console.log('%s finish', i, id);
+    }).catch(error => {
+        console.error('%s error:%s', i, error.message);
+    });
+}
